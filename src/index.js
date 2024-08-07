0eee8850b0e1cb1a44b9f855e2348ad32225e528
Darya Kolenchenko <daria.kolenchenko@gmail.com>
2024-08-07 17:09:50 +0300

    init: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const obj = {};
+  for (let bracket of bracketsConfig){
+    obj[bracket[0]] = bracket[1];
+  }
+
+  const arr = [];
+  for (let char of str) {
+    const isOpening = Object.keys(obj).includes(char);
+    const isClosing = Object.values(obj).includes(char);
+    if (isOpening && isClosing) {
+        const opening = arr.at(-1);
+        if (opening == char) {
+            arr.pop();
+        } else {
+            arr.push(char);
+        }
+    } else if (isOpening) {
+        arr.push(char);
+    } else if (isClosing) {
+        const opening = arr.pop();
+        if (obj[opening] !== char) return false;
+    } else {
+        return false;
+    }
+  }
+  return arr.length == 0;
 }
